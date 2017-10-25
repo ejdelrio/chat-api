1e750e885e89790c44b77ebaa9ab222e2ffb72b0
Eddie <Eddie@Edwins-MacBook-Pro.local>
2017-10-25 00:23:18 -0700

    Created a setInterval function that keeps track of users last key stroke and unrenders the typing status icon when the timer runs out. Every new keystroke resets the timer

--- router/socket-router.js
+++ router/socket-router.js
@@ -9,9 +9,34 @@ module.exports = server => {
   websocket.on('connection', (socket) => {
     console.log('__SOCKET_CONNECTION__: ', socket.id);
 
+    let intervalBoolean = false;
+    let timer = 3;
+
+    socket.on('clearTimer', () => timer = 0);
     socket.on('sendTyping', data => {
-      let {convoNode} = data;
-      if(convoNode) websocket.sockets.emit(`showTyping-${convoNode.convoHubID}`, data);
+
+      timer = 3;
+
+
+      if(!intervalBoolean) {
+
+        intervalBoolean = true;
+        let {convoNode} = data;
+        data.seeTyping = true;
+        if(convoNode) websocket.sockets.emit(`showTyping-${convoNode.convoHubID}`, data);
+
+        let countDown = setInterval(() => {
+          if (timer > 0) timer -= 1;
+          if (timer === 0) {
+            intervalBoolean = false;
+            timer = 3;
+            data.seeTyping = false;
+            if(convoNode) websocket.sockets.emit(`showTyping-${convoNode.convoHubID}`, data);
+            clearInterval(countDown);
+          }
+        }, 1000);
+
+      }
     })
   });
   return websocket;
